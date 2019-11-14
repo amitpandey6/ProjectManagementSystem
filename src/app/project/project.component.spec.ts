2f5f011fb2e4f8c5ed17d629dde634b4a99add85
amitpandey6 <57348428+amitpandey6@users.noreply.github.com>
2019-11-14 15:46:47 +0530

    Added Project Component

---
+++ src/app/project/project.component.spec.ts
@@ -0,0 +1,168 @@
+﻿
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectComponent } from './project.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ManageTaskService } from 'src/app/ManageTask.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
+import { Project } from 'src/app/Projectinfo.model';
+import { User } from 'src/app/User.model';
+import { Task } from 'src/app/Task.model';
+
+export class ProjectMockService {
+
+    ProjectInfoList: Array<Project> = new Array<Project>();
+
+    GetProjectList() {
+        let ProjectInfoList: Array<Project> = new Array<Project>();
+        for (var i = 1; i <= 10; i++) {
+            let obj = new Project();
+            obj.Project_ID = i;
+            obj.Project = "Project - " + i;
+            obj.Start_Date = new Date();
+            obj.End_Date = new Date();
+            obj.Priority = i + 1;
+            obj.Userinfo = new User();
+            obj.Userinfo.User_ID = i;
+            obj.Userinfo.First_Name = "First Name - " + i;
+            obj.Userinfo.Last_Name = "Last Name - " + i;
+            obj.Userinfo.Employee_ID = "Emp - " + i;
+
+            obj.User_ID = i;
+
+            ProjectInfoList.push(obj);
+        }
+        this.ProjectInfoList = ProjectInfoList;
+        return of(this.ProjectInfoList);
+    }
+
+    GetTasksList() {
+        let TaskList: Array<Task> = new Array<Task>();
+        for (var i = 1; i <= 10; i++) {
+            let obj = new Task();
+            obj.Task_ID = i;
+            obj.Project_ID = i;
+            TaskList.push(obj);
+        }
+        return of(TaskList);
+
+    }
+
+    AddUpdateProject(model: Project) {
+        let ProjectInfoList = this.ProjectInfoList;
+        if (model.Project_ID > 0) {
+            let obj = ProjectInfoList.find(x => x.Project_ID == model.Project_ID)
+            obj.Project = "Project - updated";
+            obj.CompletedTask = 25;
+            obj.Start_Date = new Date();
+            obj.End_Date = new Date();
+            obj.Priority = 4;
+            obj.NoOfTask = 6;
+        }
+        else {
+            ProjectInfoList.push(model);
+        }
+        this.ProjectInfoList = ProjectInfoList;
+        return of(this.ProjectInfoList);
+    }
+
+    DeleteProject(id: number) {
+        let ProjectInfoList = this.ProjectInfoList;
+        let index = ProjectInfoList.findIndex(x => x.Project_ID == id)
+        ProjectInfoList.splice(index, 1);
+        this.ProjectInfoList = ProjectInfoList;
+        return of(this.ProjectInfoList);
+    }
+}
+
+describe('ProjectComponent', () => {
+    let component: ProjectComponent;
+    let fixture: ComponentFixture<ProjectComponent>;
+    let MockService = new ProjectMockService();
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule, HttpClientTestingModule, RouterTestingModule.withRoutes([{ path: 'view', component: ProjectComponent }])],
+            providers: [{ provide: ManageTaskService, useValue: MockService }],
+            declarations: [ProjectComponent]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProjectComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('Project Create component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Project Load Test', () => {
+        component.LoadProjectList();
+        expect(component.ProjectInfoList.length).toBe(10);
+    });
+
+    it('Project Add Test', () => {
+        let obj = new Project();
+        obj.Project_ID = 0;
+        obj.Project = "Project - 11";
+        obj.CompletedTask = 5;
+        obj.Start_Date = new Date();
+        obj.End_Date = new Date();
+        obj.Priority = 6;
+        obj.NoOfTask = 7;
+        obj.Userinfo = new User();
+        obj.Userinfo.User_ID = 6;
+        obj.Userinfo.First_Name = "First Name - 6";
+        obj.Userinfo.Last_Name = "Last Name - 6";
+        obj.Userinfo.Employee_ID = "Emp - 6";
+        obj.User_ID = 6;
+        component.ProjectInfo = obj;
+        component.OnAddUpdate();
+        expect(component.MessageInfo).toContain("added");
+    });
+
+    it('Project Update Test', () => {
+        let obj = new Project();
+        obj.Project_ID = 5;
+        obj.Project = "Project - 5";
+        obj.CompletedTask = 5;
+        obj.Start_Date = new Date();
+        obj.End_Date = new Date();
+        obj.Priority = 6;
+        obj.NoOfTask = 7;
+        obj.Userinfo = new User();
+        obj.Userinfo.User_ID = 6;
+        obj.Userinfo.First_Name = "First Name - 6";
+        obj.Userinfo.Last_Name = "Last Name - 6";
+        obj.Userinfo.Employee_ID = "Emp - 6";
+        obj.User_ID = 6;
+        component.ProjectInfo = obj;
+        component.OnAddUpdate();
+        expect(component.MessageInfo).toContain("Updated");
+    });
+
+    it('Project Delete Test', () => {
+        component.OnDelete(1);
+        expect(component.MessageInfo).toBe("Project Information Deleted sucessfully");
+    });
+
+    it('Project Reset Test', () => {
+        component.OnResetInfo();
+        expect(component.ProjectInfo.User_ID).toBe(undefined);
+    });
+
+    it('Project Search Test', () => {
+        component.SearchValue = "Project - 1";
+        component.OnSearchInfo();
+        expect(component.ProjectInfoList[0].Project).toBe("Project - 1");
+    });
+
+
+});
+
+
---
+++ src/app/project/project.component.ts
@@ -0,0 +1,214 @@
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
+import { Project } from 'src/app/ProjectInfo.model'
+import { ManageTaskService } from 'src/app/ManageTask.service';
+import { NgForm, NgModel } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/User.model'
+import { Task } from 'src/app/Task.model'
+
+@Component({
+  selector: 'app-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+
+export class ProjectComponent implements OnInit {
+
+    public ProjectInfoList: Project[];
+
+    public ProjectInfo: Project;
+
+    public UserInfoList: User[];
+    public ProjectInfoListFromService: Project[];
+    public ActionText = "Add";
+    public MessageInfo = "";
+    public MessageType = "";
+    public SortBy = "";
+    public SearchValue = "";
+    public SetDate = false;
+    public UserName = "";
+
+    @ViewChild('ManagerModel', { static: false }) ManagerModel;
+    public SelectedUserId: number;
+
+    constructor(public service: ManageTaskService, private router: Router) { }
+
+    ngOnInit() {
+        this.ProjectInfo = new Project();
+        this.LoadProjectList();
+    }
+    OnAddUpdate() {
+        if (this.ProjectInfo.Project == undefined || this.ProjectInfo.Project.length <= 0) {
+            this.MessageType = "Error";
+            this.MessageInfo = "please enter project";
+        }
+        else if (this.ProjectInfo.Start_Date == undefined) {
+            this.MessageType = "Error";
+            this.MessageInfo = "please enter Start Date";
+        }
+        else if (this.ProjectInfo.End_Date == undefined) {
+            this.MessageType = "Error";
+            this.MessageInfo = "please enter End Date";
+        }
+        else if (this.ProjectInfo.User_ID == undefined) {
+            this.MessageType = "Error";
+            this.MessageInfo = "please enter Manager";
+        }
+        else {
+
+            this.service.AddUpdateProject(this.ProjectInfo).subscribe((data: any) => {
+                this.MessageType = "Sucesses";
+                if (this.ProjectInfo.Project_ID > 0) {
+                    this.MessageInfo = "Project Information Updated sucessfully";
+                }
+                else {
+                    this.MessageInfo = "Project Information added sucessfully";
+                }
+                this.ProjectInfo = new Project();
+                this.SetDate = false;
+                this.SearchValue = "";
+                this.SortBy = "";
+                this.UserName = "";
+                this.LoadProjectList();
+            });
+        }
+
+    }
+    OnResetInfo() {
+        this.ActionText = "Add";
+        this.MessageType = "";
+        this.MessageInfo = "";
+        this.ProjectInfo = new Project();
+        this.SearchValue = "";
+        this.SortBy = "";
+        this.ProjectInfoList = this.ProjectInfoListFromService;
+        this.SetDate = false;
+
+    }
+    OnManagerSearchOpen() {
+        this.SelectedUserId = undefined;
+        if (this.UserInfoList == undefined) {
+            this.service.GetUserList().subscribe((data: any) => {
+                this.UserInfoList = data;
+            });
+        }
+        this.ManagerModel.nativeElement.className = 'modal show';
+    }
+    OnManagerSearchSave() {
+        if (this.SelectedUserId != undefined) {
+            this.ProjectInfo.Userinfo = this.UserInfoList.find(x => x.User_ID == this.SelectedUserId);
+            this.ProjectInfo.User_ID = this.SelectedUserId;
+            this.UserName = this.ProjectInfo.Userinfo.First_Name;
+            this.OnManagerSearchClose();
+        }
+    }
+    OnManagerSearchClose() {
+        this.ManagerModel.nativeElement.className = 'modal hide';
+    }
+
+
+    LoadProjectList() {
+        this.service.GetProjectList().subscribe((data: any) => {
+            this.ProjectInfoListFromService = data;
+
+            this.service.GetTasksList().subscribe((tskData: any) => {
+                let tskList = tskData as Task[];
+                this.ProjectInfoListFromService.forEach(proj => {
+                    proj.NoOfTask = tskList.filter(x => x.Project_ID == proj.Project_ID).length;
+
+                    proj.CompletedTask = tskList.filter(x => x.Project_ID == proj.Project_ID && x.IsTaskEnded == 1).length;
+                });
+                this.ProjectInfoList = this.ProjectInfoListFromService;
+            });
+        });
+    }
+    OnSort(SortBy: string) {
+        this.MessageInfo = "";
+        this.MessageType = "";
+        this.SortBy = SortBy;
+        this.SortProjectInfo();
+    }
+    OnSearchInfo() {
+        if (this.SearchValue != undefined || this.SearchValue != "") {
+            this.ProjectInfoList = this.ProjectInfoListFromService;
+            this.ProjectInfoList = this.ProjectInfoList.filter(x => x.Project.startsWith(this.SearchValue)
+            );
+        }
+    }
+    OnEdit(id: number) {
+        this.ActionText = "Update";
+        this.ProjectInfo = this.ProjectInfoList.find(x => x.Project_ID == id);
+        this.SetDate = this.ProjectInfo.Start_Date != undefined || this.ProjectInfo.End_Date != undefined;
+    }
+
+    OnDelete(id: number) {
+        this.service.DeleteProject(id).subscribe((data: any) => { this.LoadProjectList(); });
+        this.MessageInfo = "Project Information Deleted sucessfully";
+
+    }
+    OnSetDate() {
+        if (!this.SetDate) {
+            this.ProjectInfo.Start_Date = new Date();
+            var EndDate = new Date();
+            EndDate.setDate(EndDate.getDate() + 1);
+            this.ProjectInfo.End_Date = EndDate;
+        }
+        else {
+            this.ProjectInfo.Start_Date = null;
+            this.ProjectInfo.End_Date = null;
+        }
+    }
+
+    SortProjectInfo() {
+        if (this.SortBy == "StartDate") {
+            this.ProjectInfoList.sort((a, b) => {
+                if (a.Start_Date > b.Start_Date) {
+                    return 1;
+                }
+                else if (a.Start_Date < b.Start_Date) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+        } else if (this.SortBy == "EndDate") {
+            this.ProjectInfoList.sort((a, b) => {
+                if (a.End_Date > b.End_Date) {
+                    return 1;
+                }
+                else if (a.End_Date < b.End_Date) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+        } else if (this.SortBy == "Priority") {
+            this.ProjectInfoList.sort((a, b) => {
+                if (a.Priority > b.Priority) {
+                    return 1;
+                }
+                else if (a.Priority < b.Priority) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+        } else {
+            this.ProjectInfoList.sort((a, b) => {
+                if (a.CompletedTask > b.CompletedTask) {
+                    return 1;
+                }
+                else if (a.CompletedTask < b.CompletedTask) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+        }
+    }
+}
