71b4c08263ff1ff2e1568a7657baf796a4efddd5
amitpandey6 <57348428+amitpandey6@users.noreply.github.com>
2019-11-14 15:51:28 +0530

    Add files via upload

---
+++ src/app/view-task/view-task.component.spec.ts
@@ -0,0 +1,86 @@
+﻿import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewTaskComponent } from './view-task.component';
+import { ManageTaskService } from 'src/app/ManageTask.service';
+import { Task } from 'src/app/Task.model';
+import { Parenttask } from 'src/app/parenttask.model';
+import { Observable, from } from 'rxjs';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { Project } from 'src/app/ProjectInfo.model'
+import { User } from 'src/app/User.model';
+
+
+describe('ViewTaskComponent', () => {
+    let component: ViewTaskComponent;
+    let fixture: ComponentFixture<ViewTaskComponent>;
+    let service: ManageTaskService;
+    let rout: Router;
+
+    beforeEach(() => {
+        service = new ManageTaskService(null);
+        component = new ViewTaskComponent(service, rout);
+    });
+
+    it('should set task with task returned', () => {
+        // Arrange
+        let parenttask = new Parenttask();
+        parenttask.Parent_ID = 1;
+        parenttask.Parent_Task = "parent1";
+        parenttask.ParentTaskType = "Parent";
+
+        let userinfo = new User();
+        userinfo.User_ID = 1;
+        userinfo.First_Name = "Amit";
+        userinfo.Last_Name = "Pandey";
+        userinfo.Employee_ID = "1";
+
+        let proj = new Project();
+        proj.Project_ID = 1;
+        proj.Project = "project1";
+        proj.Start_Date = new Date("2019-11-14");
+        proj.End_Date = new Date("2019-13-14");
+        proj.Priority = 20;
+        proj.User_ID = 1;
+        proj.Userinfo = userinfo;
+        proj.NoOfTask = 4;
+        proj.CompletedTask = 3;
+
+        const tasks: Task[] = [
+            {
+                Task_ID: 1,
+                TaskName: "task1",
+                Start_Date: new Date("2019-11-14"),
+                End_Date: new Date("2021-01-01"),
+                Priority: 20,
+                ParentTaskType: "parent1",
+                Parent_ID: 1,
+                IsTaskEnded: 0,
+                ParentTask: parenttask,
+                Project: proj,
+                Project_ID: 1
+            },
+            {
+                Task_ID: 2,
+                TaskName: "task2",
+                Start_Date: new Date("2018-05-02"),
+                End_Date: new Date("2019-05-02"),
+                Priority: 27,
+                ParentTaskType: "parent1",
+                Parent_ID: 1,
+                IsTaskEnded: 0,
+                ParentTask: parenttask,
+                Project: proj,
+                Project_ID: 1
+            }
+        ];
+        spyOn(service, "GetTasksList").and.callFake(() => {
+            return from([tasks]);
+        });
+        // Act
+        component.ngOnInit();
+
+        //Assert
+        expect(component.taskinfo).toEqual(tasks);
+    });
+});
---
+++ src/app/view-task/view-task.component.ts
@@ -0,0 +1,137 @@
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
+import { Task } from 'src/app/Task.model'
+import { ManageTaskService } from 'src/app/ManageTask.service';
+import { Router } from '@angular/router';
+import { Project } from 'src/app/ProjectInfo.model'
+
+@Component({
+  selector: 'app-view-task',
+  templateUrl: './view-task.component.html',
+  styleUrls: ['./view-task.component.css']
+})
+
+export class ViewTaskComponent implements OnInit {
+    public taskinfo: Task[];
+    public tskInfo: Task;
+    public TaskinfoFromService: Task[];
+    public ProjectInfoList: Project[];
+    public TaskName: string;
+    public ParentTask: string;
+    public PriorityFrom: number;
+    public PriorityTo: number;
+    public StartDate: Date;
+    public EndDate: Date;
+    private SortBy = "";
+    public SelectedProjectId: number;
+    public ProjectName = "";
+    @ViewChild('ProjectModel', { static: false }) ProjectModel;
+
+    constructor(public service: ManageTaskService, public router: Router) { }
+
+    ngOnInit() {
+        this.LoadTask();
+    }
+
+    LoadTask() {
+        this.service.GetTasksList().subscribe((data: any) => {
+            this.TaskinfoFromService = data;
+            this.taskinfo = data;
+        });
+    }
+    deleteTask(tskid: number) {
+        this.service.EndTask(tskid).subscribe((data: any) => { this.LoadTask(); });
+    }
+    editTask(id: number) {
+        this.router.navigate([`./Add/${id}`]);
+    }
+    OnSort(SortBy: string) {
+        this.SortBy = SortBy;
+        this.SortTaskInfo();
+    }
+    SortTaskInfo() {
+        if (this.SortBy == "StartDate") {
+            this.taskinfo.sort((a, b) => {
+                if (a.Start_Date > b.Start_Date) {
+                    return 1;
+                }
+                else if (a.Start_Date < b.Start_Date) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+        } else if (this.SortBy == "EndDate") {
+            this.taskinfo.sort((a, b) => {
+                if (a.End_Date > b.End_Date) {
+                    return 1;
+                }
+                else if (a.End_Date < b.End_Date) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+        } else if (this.SortBy == "Priority") {
+            this.taskinfo.sort((a, b) => {
+                if (a.Priority > b.Priority) {
+                    return 1;
+                }
+                else if (a.Priority < b.Priority) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+        } else if (this.SortBy == "Completed") {
+            this.taskinfo.sort((a, b) => {
+                if (a.IsTaskEnded > b.IsTaskEnded) {
+                    return 1;
+                }
+                else if (a.IsTaskEnded < b.IsTaskEnded) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+        }
+
+    }
+    OnProjectSearchOpen() {
+        this.SelectedProjectId = undefined;
+        if (this.ProjectInfoList == undefined) {
+            this.service.GetProjectList().subscribe((data: any) => {
+                this.ProjectInfoList = data;
+            });
+        }
+        this.ProjectModel.nativeElement.className = 'modal show';
+    }
+    OnProjectSearchClose() {
+        this.ProjectModel.nativeElement.className = 'modal hide';
+    }
+    OnProjectSearchSave() {
+        if (this.SelectedProjectId != undefined) {
+            this.tskInfo = new Task();
+            this.tskInfo.Project = this.ProjectInfoList.find(x => x.Project_ID == this.SelectedProjectId);
+            this.tskInfo.Project_ID = this.SelectedProjectId;
+            this.ProjectName = this.tskInfo.Project.Project;
+            this.OnProjectSearchClose();
+            this.SearchChange();
+        }
+    }
+    SearchChange() {
+        this.taskinfo = this.TaskinfoFromService;
+        if (this.ProjectName != undefined && this.SelectedProjectId != undefined) {
+            this.taskinfo = this.taskinfo.filter(x => x.Project_ID == this.SelectedProjectId);
+        }
+    }
+
+
+}
