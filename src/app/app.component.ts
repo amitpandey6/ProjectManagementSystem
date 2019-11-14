db372616f01d292f867f207c96a5115ec74fefb9
amitpandey6 <57348428+amitpandey6@users.noreply.github.com>
2019-11-14 15:52:17 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
+import { ProjectComponent } from './project/project.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+const approutes: Routes = [
+    { path: '', component: ViewTaskComponent },
+    { path: 'Add/:id', component: TaskComponent },
+    { path: 'view', component: ViewTaskComponent },
+    { path: 'addUser', component: UserComponent },
+    { path: 'addProject', component: ProjectComponent },
+
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+	HeaderComponent,
+	UserComponent,
+	TaskComponent,
+	ProjectComponent,
+	ViewTaskComponent
+  ],
+  imports: [
+      BrowserModule,
+      HttpClientModule,
+      RouterModule.forRoot(approutes),
+      FormsModule,
+      ReactiveFormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'ProjectManagementApp';
-}
+export class AppModule { }
