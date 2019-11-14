3b89ccf228e6d8679134b73c5e8544c865364234
amitpandey6 <57348428+amitpandey6@users.noreply.github.com>
2019-11-14 15:48:47 +0530

    Add files via upload

---
+++ src/app/user/user.component.spec.ts
@@ -0,0 +1,135 @@
+﻿import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+
+import { UserComponent } from './user.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ManageTaskService } from 'src/app/ManageTask.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { User } from 'src/app/User.model'
+import { Observable, of } from 'rxjs';
+
+export class UserMockService {
+
+    UserInfoList1: Array<User> = new Array<User>();
+
+    GetUserList() {
+        let UserInfoList: Array<User> = new Array<User>();
+        for (var i = 1; i <= 10; i++) {
+            let objuser = new User();
+            objuser.User_ID = i;
+            objuser.First_Name = "First Name - " + i;
+            objuser.Last_Name = "Last Name - " + i;
+            objuser.Employee_ID = "Emp - " + i;
+            UserInfoList.push(objuser);
+        }
+        this.UserInfoList1 = UserInfoList;
+        return of(this.UserInfoList1);
+    }
+
+    GetUserInfo() { return new User(); }
+
+    AddUpdateUser(model: User) {
+        let UserInfoList = this.UserInfoList1;
+        if (model.User_ID > 0) {
+            let objinfo = UserInfoList.find(x => x.User_ID == model.User_ID)
+            objinfo.First_Name = model.First_Name;
+            objinfo.Last_Name = model.Last_Name;
+            objinfo.Employee_ID = model.Employee_ID;
+        }
+        else {
+            UserInfoList.push(model);
+        }
+        this.UserInfoList1 = UserInfoList;
+        return of(this.UserInfoList1);
+    }
+
+    DeleteUser(id: number) {
+        let UserInfoList = this.UserInfoList1;
+        let index = UserInfoList.findIndex(x => x.User_ID == id)
+        UserInfoList.splice(index, 1);
+        this.UserInfoList1 = UserInfoList;
+        return of(this.UserInfoList1);
+    }
+}
+
+describe('UserinfoComponent', () => {
+    let component: UserComponent;
+    let fixture: ComponentFixture<UserComponent>;
+    let userMockService = new UserMockService();
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule, HttpClientTestingModule, RouterTestingModule.withRoutes([{ path: 'view', component: UserComponent }])],
+            declarations: [UserComponent],
+            providers: [{ provide: ManageTaskService, useValue: userMockService }]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+
+    it('User Create component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('User Load Test', () => {
+        component.LoadUser();
+        expect(component.UserInfoList.length).toBe(10);
+    });
+
+    it('User Add Test', () => {
+        component.UserInfo = new User();
+        component.UserInfo.User_ID = 0;
+        component.UserInfo.First_Name = "First Name - 11";
+        component.UserInfo.Last_Name = "Last Name - 11";
+        component.UserInfo.Employee_ID = "Emp - 11";
+        component.AddUpdateUser();
+        expect(component.MessageInfo).toBe("User information added sucessfully");
+    });
+    it('User Update Test', () => {
+        component.UserInfo = new User();
+        component.UserInfo.User_ID = 1;
+        component.UserInfo.First_Name = "Updated First Name - 5";
+        component.UserInfo.Last_Name = "Updated Last Name - 5";
+        component.UserInfo.Employee_ID = "Updated Emp - 5";
+        component.AddUpdateUser();
+        expect(component.MessageInfo).toBe("User information Updated sucessfully");
+    });
+    it('User Delete Test', () => {
+        component.DeleteUser(5);
+        expect(component.MessageInfo).toBe("User information deleted sucessfully");
+    });
+    it('User Reset Test', () => {
+        component.ResetUser();
+        expect(component.UserInfo.First_Name).toBe(null);
+    });
+
+    it('User Search Test', () => {
+        component.SearchValue = "First Name - 1";
+        component.SearchUser();
+        expect(component.UserInfoList.filter(x => x.First_Name.startsWith("First Name - 1"))).not.toBe(null);
+    });
+    it('User Sort First Name Test', () => {
+        component.SortBy = "FirstName";
+        component.SortUser("");
+        expect(component.UserInfoList[0].First_Name).toBe("First Name - 1");
+    });
+
+    it('User Sort Last Name Test', () => {
+        component.SortBy = "LastName";
+        component.SortUser("");
+        expect(component.UserInfoList[0].Last_Name).toBe("Last Name - 1");
+    });
+
+    it('User Sort Employee ID Test', () => {
+        component.SortBy = "EMPID";
+        component.SortUser("");
+        expect(component.UserInfoList[0].Employee_ID).toBe("Emp - 1");
+    });
+
+});
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,140 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/User.model'
+import { ManageTaskService } from 'src/app/ManageTask.service';
+import { NgForm, NgModel } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+
+export class UserComponent implements OnInit {
+    public UserInfoList: User[];
+    public UserInfo: User;
+    public ActionText = "Add";
+    public UserInfoListFromService: User[];
+    public MessageInfo = "";
+    public MessageType = "";
+    public SortBy = "";
+    public SearchValue = "";
+
+
+    constructor(public service: ManageTaskService) { }
+
+    ngOnInit() {
+        this.UserInfo = new User();
+        this.LoadUser();
+    }
+    AddUpdateUser() {
+        if (this.UserInfo.First_Name == undefined || this.UserInfo.First_Name.length <= 0) {
+            this.MessageType = "Error";
+            this.MessageInfo = "please enter First Name";
+        }
+        else if (this.UserInfo.Last_Name == undefined || this.UserInfo.Last_Name.length <= 0) {
+            this.MessageType = "Error";
+            this.MessageInfo = "please enter Last Name";
+        }
+        else if (this.UserInfo.Employee_ID == undefined || this.UserInfo.Employee_ID.length <= 0) {
+            this.MessageType = "Error";
+            this.MessageInfo = "please enter Employee Id";
+        } else {
+            this.service.AddUpdateUser(this.UserInfo).subscribe((data: any) => {
+                this.MessageType = "Sucesses";
+                if (this.UserInfo.User_ID > 0) {
+                    this.MessageInfo = "User information Updated sucessfully";
+                }
+                else {
+                    this.MessageInfo = "User information added sucessfully";
+                }
+                this.UserInfo = new User();
+                this.SearchValue = "";
+                this.SortBy = "";
+                this.LoadUser();
+
+            });
+        }
+
+    }
+    LoadUser() {
+        this.service.GetUserList().subscribe((data: any) => {
+            this.UserInfoList = data;
+            this.UserInfoListFromService = data;
+        });
+
+    }
+    EditUser(id: number) {
+        this.ActionText = "Update"
+        this.UserInfo = this.UserInfoList.find(x => x.User_ID == id);
+
+    }
+    DeleteUser(id: number) {
+        this.service.DeleteUser(id).subscribe((data: any) => {
+            this.MessageInfo = "User information deleted sucessfully";
+            this.LoadUser();
+        });
+
+    }
+    SortUser(SortBy: string) {
+        this.MessageInfo = "";
+        this.MessageType = "";
+        this.SortBy = SortBy;
+        this.SortUserInfo();
+    }
+    SortUserInfo() {
+        if (this.SortBy == "FirstName") {
+            this.UserInfoList.sort((a, b) => {
+                if (a.First_Name > b.First_Name) {
+                    return 1;
+                }
+                else if (a.First_Name < b.First_Name) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+        } else if (this.SortBy == "LastName") {
+            this.UserInfoList.sort((a, b) => {
+                if (a.Last_Name > b.Last_Name) {
+                    return 1;
+                }
+                else if (a.Last_Name < b.Last_Name) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+        } else {
+            this.UserInfoList.sort((a, b) => {
+                if (a.User_ID > b.User_ID) {
+                    return 1;
+                }
+                else if (a.User_ID < b.User_ID) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+        }
+    }
+    SearchUser() {
+        if (this.SearchValue != undefined || this.SearchValue != "") {
+            this.UserInfoList = this.UserInfoListFromService;
+            this.UserInfoList = this.UserInfoList.filter(x => x.First_Name.startsWith(this.SearchValue)
+                || x.Last_Name.startsWith(this.SearchValue)
+                || x.Employee_ID.startsWith(this.SearchValue)
+            );
+        }
+    }
+    ResetUser() {
+        this.UserInfo.First_Name = null;
+        this.UserInfo.Last_Name = null;
+        this.UserInfo.Employee_ID = null;
+    }
+
+}
